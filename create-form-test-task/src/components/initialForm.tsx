31336265e7a0ce5fec9128d2f409690952bfc7e6
mohammadreza Razzaz <mohammadrezarzz1999@gmail.com>
2024-04-15 13:17:21 +0330

    add some style

--- create-form-test-task/src/components/initialForm.tsx
+++ create-form-test-task/src/components/initialForm.tsx
@@ -7,8 +7,7 @@ interface IProps {
 }
 
 const ProductPage: React.FC<IProps> = ({ data }) => {
-  console.log(data);
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>(data);
   const [inputValue, setInputValue] = useState("");
   const [filter, setFilter] = useState("all");
 
@@ -65,20 +64,44 @@ const ProductPage: React.FC<IProps> = ({ data }) => {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <button onClick={add}>Add Product</button>
-      <select value={filter} onChange={(e) => setFilter(e.target.value)}>
-        <option value="all">All</option>
-        <option value="archived">Archived</option>
-        <option value="active">Active</option>
-      </select>
-      <ul>
-        {/* {filteredProducts().map((product) => (
+    <div className="max-w-[1200px] mx-auto">
+      <div className="bg-slate-500 px-10 py-12 rounded-sm">
+        <div className="flex ">
+          <input
+            className="flex-1 px-6 border"
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <div className="bg-green-500 px-10 py-3 mr-10 cursor-pointer active:scale-[95%] font-semibold text-white">
+            Add
+          </div>
+          <select value={filter} onChange={(e) => setFilter(e.target.value)}>
+            <option value="all">All</option>
+            <option value="archived">Archived</option>
+            <option value="active">Active</option>
+          </select>
+        </div>
+      </div>
+
+      <ul className="bg-red-400 mt-10 px-5 flex flex-col ">
+        <li className="inline-flex items-center justify-between py-2 ">
+          <p className="text-white">{"dasdhsaghdcghsa"}</p>
+          <div className="space-x-3">
+            <span className="inline-block px-5 py-3 cursor-pointer active:scale-[95%] font-semibold text-white bg-blue-700">
+              Archived
+            </span>
+            <span className="inline-block px-5 py-3 cursor-pointer active:scale-[95%] font-semibold text-white bg-yellow-400">
+              Toggle
+            </span>
+            <span className="inline-block px-5 py-3 cursor-pointer active:scale-[95%] font-semibold text-white bg-red-700">
+              Remove
+            </span>
+          </div>
+        </li>
+      </ul>
+
+      {/* {filteredProducts().map((product) => (
           <li>
             {product.title}
             <button onClick={() => remove(product.id)}>Delete</button>
@@ -87,7 +110,6 @@ const ProductPage: React.FC<IProps> = ({ data }) => {
             </button>
           </li>
         ))} */}
-      </ul>
     </div>
   );
 };
